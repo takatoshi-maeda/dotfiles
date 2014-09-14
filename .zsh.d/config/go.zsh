fcd26a379b70deb0116743c16ee891e6fc445403
Takatoshi Maeda <me@tmd.tw>
2014-09-14 21:28:15 +0900

    Change golang environment variables

--- .zsh.d/config/go.zsh
+++ .zsh.d/config/go.zsh
@@ -1,3 +1,6 @@
-export GOROOT=`go env GOROOT`
-export GOPATH=$HOME/.go/$GO_VERSION
-export PATH=$PATH:$GOPATH/bin
+if [ -x "`which go`" ]; then
+  export GOROOT=`go env GOROOT`
+  export GO_VERSION=`go version | awk 'match($0,/[0-9]\.[0-9]\.[0-9]+/) { print substr($0,RSTART,RLENGTH) }'`
+  export GOPATH=$HOME/.go/$GO_VERSION
+  export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+fi
